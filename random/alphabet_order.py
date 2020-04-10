f6ec17b9dba4ae06d052e038879c5a625af7bd6d
Erwin Eko Wahyudi <erwin.eko@bukalapak.com>
2020-04-10 23:01:40 +0700

    add script alphabet order

---
+++ random/alphabet_order.py
@@ -0,0 +1,74 @@
+def get_minimum(a, b):
+	if a<b:
+		return a
+	else:
+		return b
+
+
+def DFS(point, order, flag, adj_list):
+	flag[point] = True
+
+	for next_point in adj_list[point]:
+		if not flag[next_point]:
+			DFS(next_point, order, flag, adj_list)
+
+	order.insert(0, point) # put in front
+
+
+def solve(words):
+	pairs = []
+	for i in range(len(words)):
+		for j in range(i+1, len(words)):
+			word1 = words[i]
+			word2 = words[j]
+			idx = -1
+			for k in range(get_minimum(len(word1), len(word2))):
+				if word1[k] != word2[k]:
+					idx = k
+					break
+
+			if idx != -1:
+				subword1 = word1[idx:len(word1)]
+				subword2 = word2[idx:len(word2)]
+
+				pair = [subword1[0], subword2[0]]
+				if (pair[0] != pair[1]) & (pair not in pairs):
+					pairs.append(pair)
+
+	# print(pairs)
+
+	# create graph, then do topological sort, with vertex is all alphabet in words
+	points = list(set("".join(words)))
+	adj_list = {point: [] for point in points}
+	flag = {point: False for point in points}
+
+	for pair in pairs:
+		adj_list[pair[0]].append(pair[1])
+
+	# do DFS for topological sort
+	order = []
+	for point in points:
+		if not flag[point]:
+			DFS(point, order, flag, adj_list)
+
+	# print(order)
+	return order
+
+
+def main():
+	# print(primes)
+	t = int(input())
+	words = []
+
+	for _ in range(t):
+		word = input()
+		words.append(word)
+
+	order = solve(words)
+
+	result = "".join(order)
+	print(result)
+
+
+if __name__ == "__main__":
+	main()
